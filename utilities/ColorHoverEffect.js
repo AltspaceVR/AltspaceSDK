371dd28e21b82de9b49dc8e5f1898afd068bcab2
Amber Roy <amber.roy@gmail.com>
2015-09-12 20:52:21 -0700

    moved Effects to altspace.utilities version 0.3.1

--- utilities/ColorHoverEffect.js
+++ utilities/ColorHoverEffect.js
@@ -1,81 +1,94 @@
 //Change color of an object when cursor hovers over it.
-ColorHoverEffect = function (hoverColor, params){
-	this.hoverColor = hoverColor;
-	this.hoverObject;
-	this.cursordownObject;
-	var p = params || {};
-	if (p.color && ! (p.color instanceof THREE.Color)){
-		console.error('Color should be instance of THREE.Color', p.color);
-		return ;//valid color required
-	}
-	this.TRACE = p.TRACE || null; 
-};
+window.altspace.utilities.ColorHoverEffect = function(){
 
-ColorHoverEffect.prototype.cursorenter = function(object){
-	//Ignore hover events if a different object is selected,
-	//for example during a drag we don't want to change highlight
-	if (this.cursordownObject && this.cursordownObject !== object){
-		if (this.TRACE) console.log('Ignore hover, other object selected', this.cursordownObject);
-		return ;
-	} 
-	if (this.hoverObject){
-		this.unhoverEffect(this.hoverObject);
-	}
-	this.hoverEffect(object);
-};
+  var hoverColor;
+  var hoverObject;
+  var cursordownObject;
+  var TRACE;
 
-ColorHoverEffect.prototype.cursorleave = function(object){
-	if (this.hoverObject === object){
-		this.unhoverEffect(object);
-	}
-};
+  function init(myHoverColor, params){
+    hoverColor = myHoverColor;
+    if (myHoverColor && ! (myHoverColor instanceof THREE.Color)){
+      throw new Error('Color should be instance of THREE.Color', p.color);
+    }
+    var p = params || {};
+    TRACE = p.TRACE || null; 
+  }
 
-ColorHoverEffect.prototype.cursordown = function(object){
-	if (this.TRACE) console.log('setting cursordownObject', object);
-	this.cursordownObject = object;
-};
+  function cursordown(object){
+    if (TRACE) console.log('setting cursordownObject', object);
+    cursordownObject = object;
+  }
 
-ColorHoverEffect.prototype.cursorupScene = function(object){
-	if (this.TRACE) console.log('clearning cursordownObject', object);
-	this.cursordownObject = null;
+  function cursorenter(object){
+    //Ignore hover events if a different object is selected,
+    //for example during a drag we don't want to change highlight
+    if (cursordownObject && cursordownObject !== object){
+      if (TRACE) console.log('Ignore hover, other object selected', cursordownObject);
+      return;
+    } 
+    if (hoverObject){
+      unhoverEffect(hoverObject);
+    }
+    hoverEffect(object);
+  }
 
-};
+  function cursorleave(object){
+    if (hoverObject === object){
+      unhoverEffect(object);
+    }
+  }
 
-ColorHoverEffect.prototype.hoverEffect = function(object){
-	this.hoverObject = object;
-	this.setHoverColor(object, this.hoverColor);
-	if (this.TRACE) console.log('hoverEffect', object);
-};
+  function cursorupScene(object){
+    if (TRACE) console.log('clearning cursordownObject', object);
+    cursordownObject = null;
 
-ColorHoverEffect.prototype.unhoverEffect = function(object){
-	this.hoverObject = null;
-	this.unsetHoverColor(object);
-	if (this.TRACE) console.log('unhoverEffect', object);
-};
+  }
 
-ColorHoverEffect.prototype.setHoverColor = function (obj, hoverColor){
-	if (obj.material && obj.material.color){
-		obj.userData.origColor = obj.material.color;
-		obj.material.color = hoverColor;	
-		//Not strictly needed but seems to make updating faster in Altspace.
-		if (obj.material) obj.material.needsUpdate = true;
-	} 
-	for (var i = 0; i < obj.children.length; i++){
-		this.setHoverColor(obj.children[i], hoverColor);//recursively apply to children
-	}
-};
+  function hoverEffect(object){
+    hoverObject = object;
+    setHoverColor(object, hoverColor);
+    if (TRACE) console.log('hoverEffect', object);
+  }
 
-ColorHoverEffect.prototype.unsetHoverColor = function (obj){
+  function unhoverEffect(object){
+    hoverObject = null;
+    unsetHoverColor(object);
+    if (TRACE) console.log('unhoverEffect', object);
+  }
 
-	if (obj.material && obj.material.color){
-		if (!obj.userData.origColor){
-			console.error('Cannot unsetHoverColor, no userData.origColor for object', obj);
-			return;
-		}
-		obj.material.color = obj.userData.origColor;
-		if (obj.material) obj.material.needsUpdate = true;
-	} 
-	for (var i = 0; i < obj.children.length; i++){
-		this.unsetHoverColor(obj.children[i]);
-	}
+  function setHoverColor(obj, hoverColor){
+    if (obj.material && obj.material.color){
+      obj.userData.origColor = obj.material.color;
+      obj.material.color = hoverColor;  
+      //Not strictly needed but seems to make updating faster in Altspace.
+      if (obj.material) obj.material.needsUpdate = true;
+    } 
+    for (var i = 0; i < obj.children.length; i++){
+      setHoverColor(obj.children[i], hoverColor);//recursively apply to children
+    }
+  }
+
+  function unsetHoverColor(obj){
+    if (obj.material && obj.material.color){
+      if (!obj.userData.origColor){
+        console.error('Cannot unsetHoverColor, no userData.origColor for object', obj);
+        return;
+      }
+      obj.material.color = obj.userData.origColor;
+      if (obj.material) obj.material.needsUpdate = true;
+    } 
+    for (var i = 0; i < obj.children.length; i++){
+      unsetHoverColor(obj.children[i]);
+    }
+  }
+
+    return {
+    init: init,
+    cursordown: cursordown,
+    cursorenter: cursorenter,
+    cursorleave: cursorleave,
+    cursorupScene: cursorupScene,
+  };
+
 };
