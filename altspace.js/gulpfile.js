99b6bb2ef2d8f67929878f209953bb16159bfd08
gavanwilhite <gavan@altvr.com>
2015-10-06 19:43:36 -0700

    changed bin to dist

--- altspace.js/gulpfile.js
+++ altspace.js/gulpfile.js
@@ -68,7 +68,7 @@ gulp.task('altspace_js', function () {
             console.log(e);
         })
         .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./bin/latest', { cwd: cwd }))
-        .pipe(gulp.dest('./bin/' + version + '/', { cwd: cwd }))
+        .pipe(gulp.dest('./dist/latest', { cwd: cwd }))
+        .pipe(gulp.dest('./dist/' + version + '/', { cwd: cwd }))
         .pipe(print());
 });
