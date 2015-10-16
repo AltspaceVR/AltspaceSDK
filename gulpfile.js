256114f8a7a8b60d28e3b7fbbd6414efc93077bc
Amber Roy <amber.roy@gmail.com>
2015-10-16 15:52:23 -0700

    Created ChangeColor behavior
    Extracted the code from spinning-cube example and put into standalone
    behavior.  Demonstrates how to use syncData to share arbitrary values
    such as color.

--- gulpfile.js
+++ gulpfile.js
@@ -83,6 +83,7 @@ gulp.task('altspace_js', function () {
             './src/utilities/behaviors/ButtonStateStyle.js',
             './src/utilities/behaviors/Drag.js',
             './src/utilities/behaviors/Spin.js',
+            './src/utilities/behaviors/ChangeColor.js',
         ], { cwd: cwd }),
         browserify(
             './src/utilities/behaviors/Layout.js'
