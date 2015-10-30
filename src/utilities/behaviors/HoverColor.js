50e75eb040e737d7fc1deb04281032b47741b21e
Amber Roy <amber.roy@gmail.com>
2015-10-29 23:07:41 -0700

    Add option to change color while cursordown
    Default to change color on hover.

--- src/utilities/behaviors/HoverColor.js
+++ src/utilities/behaviors/HoverColor.js
@@ -9,93 +9,93 @@ altspace.utilities.behaviors.HoverColor = function(config){
   config = config || {};
 
   if (config.scene === undefined) throw Error('Must pass config.scene');//TODO: better way to get the scene
+  if (config.event === undefined) config.event = 'cursorenter';
+  if (config.event !== 'cursorenter' && config.event !== 'cursordown') {
+    throw Error('Expected config.event "cursorenter" or "cursordown"');
+  }
   if (config.color === undefined) config.color = new THREE.Color('yellow');
   if (config.TRACE === undefined) config.TRACE = false;
 
   var object3d;
   var cursordownObject;
-  var hoverObject;
-  var hoverColor = config.color;
+  var cursorenterObject;
   var TRACE = config.TRACE;
 
 
   function awake(o) {
     object3d = o;
-    object3d.addEventListener("cursordown", cursordown);
-    object3d.addEventListener("cursorenter", cursorenter);
-    object3d.addEventListener("cursorleave", cursorleave);
-    config.scene.addEventListener("cursorup", cursorupScene);
+    object3d.addEventListener('cursordown', cursordown);
+    config.scene.addEventListener('cursorup', cursorupScene);
+    if (config.event === 'cursorenter') {
+      object3d.addEventListener('cursorenter', cursorenter);
+      object3d.addEventListener('cursorleave', cursorleave);
+    }
   }
 
   function cursordown(event){
     if (TRACE) console.log('setting cursordownObject', object3d);
     cursordownObject = object3d;
+    if (config.event === 'cursordown' ){
+      setColor(cursordownObject);
+    }
   }
 
   function cursorenter(event){
-    //Ignore hover events if a different object is selected,
+    //ignore hover events if a different object is selected,
     //for example during a drag we don't want to change highlight
     if (cursordownObject && cursordownObject !== object3d){
-      if (TRACE) console.log('Ignore hover, other object selected', cursordownObject);
+      if (trace) console.log('ignore hover, other object selected', cursordownObject);
       return;
     } 
-    if (hoverObject){
-      unhoverEffect(hoverObject);
+    if (cursorenterObject){
+      unsetcolor(cursorenterObject);
     }
-    hoverEffect(object3d);
+    cursorenterObject = object3d;
+    setColor(object3d);
   }
 
   function cursorleave(event){
-    if (hoverObject === object3d){
-      unhoverEffect(object3d);
+    if (cursorenterObject === object3d){
+      cursorenterObject = null;
+      unsetColor(object3d);
     }
   }
 
   function cursorupScene(event){
     if (TRACE) console.log('clearning cursordownObject', cursordownObject);
+    if (config.event === 'cursordown' && cursordownObject ){
+      unsetColor(cursordownObject);
+    }
     cursordownObject = null;
-
   }
 
-  function hoverEffect(o){
-    hoverObject = o;
-    setHoverColor(o, hoverColor);
-    if (TRACE) console.log('hoverEffect', o);
-  }
-
-  function unhoverEffect(o){
-    hoverObject = null;
-    unsetHoverColor(o);
-    if (TRACE) console.log('unhoverEffect', o);
-  }
-
-  function setHoverColor(o, hoverColor){
+  function setColor(o){
+    if (TRACE) console.log('setColor', o);
     if (o.material && o.material.color){
       o.userData.origColor = o.material.color;
-      o.material.color = hoverColor;  
+      o.material.color = config.color;  
       //Not strictly needed but seems to make updating faster in Altspace.
       if (o.material) o.material.needsUpdate = true;
     } 
     for (var i = 0; i < o.children.length; i++){
-      setHoverColor(o.children[i], hoverColor);//recursively apply to children
+      setColor(o.children[i], config.color);//recursively apply to children
     }
   }
 
-  function unsetHoverColor(o){
+  function unsetColor(o){
+    if (TRACE) console.log('unsetColor', o);
     if (o.material && o.material.color){
       if (!o.userData.origColor){
-        console.error('Cannot unsetHoverColor, no userData.origColor for object', o);
+        console.error('Cannot unsetColor, no userData.origColor for object', o);
         return;
       }
       o.material.color = o.userData.origColor;
       if (o.material) o.material.needsUpdate = true;
     } 
     for (var i = 0; i < o.children.length; i++){
-      unsetHoverColor(o.children[i]);
+      unsetColor(o.children[i]);
     }
   }
-
-  function update(){}
 
   return {
     awake: awake,
