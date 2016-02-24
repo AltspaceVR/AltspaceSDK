6d171f4551e155e17a3554998077730e6c06b08b
Amber Roy <amber.roy@gmail.com>
2016-02-24 15:17:54 -0800

    sync improvement: added getSpaceRef
    Depreciated getInstance, renamed to getInstanceRef

--- src/utilities/sync.js
+++ src/utilities/sync.js
@@ -16,6 +16,7 @@
 altspace.utilities.sync = (function() {
     
     var instance;
+    var spaceId; //only set when getSpaceRef is called.
 
     function dashEscapeFirebaseKey(keyName) {
         return encodeURIComponent(keyName).replace(/\./g, '%2E').replace(/%[A-Z0-9]{2}/g, '-');
@@ -27,6 +28,12 @@ altspace.utilities.sync = (function() {
     }
 
     function getInstance(params) {
+
+      console.warn('altspace.utilities.sync.getInstance is depreciated, use getInstanceRef instead.');
+      return getInstanceRef(params);
+    }
+
+    function getInstanceRef(params) {
         var canonicalUrl = getCanonicalUrl();
         var url = new Url();
 
@@ -54,6 +61,58 @@ altspace.utilities.sync = (function() {
         return firebaseInstance;
     }
 
+    /**
+     * Returns a promise whose argument is a firebase instance, specific to 
+     * this app and this Altspace space. Promise rejected if not in Altspace
+     * (unless spaceId given in the URL).
+     * @method getSpaceRef
+     * @param {Object} params Same as getInstanceRef
+     * @return {Promise}
+     * @memberof module:altspace/utilities/sync
+     * @example
+     *    var params = { appId: yourAppName, authorId: yourAuthorId };
+     *    altspace.utilities.sync.getSpaceRef(params).then(onConnected);
+     *    function onConnected(firebaseRef) {
+     *        firebaseRef.child('highscore').set(42);
+     *        // or create a SceneSync using firebaseRef as the syncInstance 
+     *    }
+     */
+    function getSpaceRef(params) {
+
+      params = params || {};
+      var inAltspace = altspace && altspace.inClient;
+      var hasSpaceId = window.location.search.indexOf('spaceId=') !== -1;
+
+      var p = new Promise(function(resolve, reject) {
+        if (hasSpaceId) {
+          //spaceId is given in the URL. Needed if in an iframe, cannot call
+          //getSpaceRef directly, instead set iframe url query param 'spaceId'
+          //using getSpaceId. Can also enable testing outside of Altspace.
+          var url = new Url();
+          spaceId = url.query['spaceId'];
+          console.log("Got spaceId from URL: " + spaceId);
+          params["instanceId"] = spaceId;
+          instance = getInstanceRef(params);
+          resolve(instance);
+        } else if (!inAltspace) {//error if not in altspace
+          var msg = 'Called getSpaceRef but not running in Altspace';
+          reject(msg);
+        } else {//get space id using altspace API
+          altspace.getSpace().then(function(spaceInfo) {
+            spaceId = spaceInfo.sid;
+            console.log("Got spaceId from Altspace " + spaceId);
+            params["instanceId"] = spaceId;
+            instance = getInstanceRef(params);
+            resolve(instance);
+          }, function(err) {
+            var msg = 'Error trying to get space info from Altspace: ' + err;
+            reject(msg);
+          });
+        }      
+      });
+      return p;
+    }
+
     function authenticate(callback){//TODO: Promise and document
         var ref = instance || getInstance(params);
         ref.authAnonymously(function(error, authData) {
@@ -69,7 +128,7 @@ altspace.utilities.sync = (function() {
      * Returns a firebase instance, just as if you had called new Firebase()  
      *
      * By using syncInstance.parent() you can store cross-instance data like high scores. Likewise you can store persistent user data at syncInstance.parent().child([userId).
-     * @method getInstance
+     * @method getInstanceRef
      * @param {Object} params
      * @param {String} params.appId An identifier for your app.
      * @param {String} [params.instanceId] An id for a particular instance of
@@ -79,7 +138,7 @@ altspace.utilities.sync = (function() {
      * @return {Firebase}
      * @memberof module:altspace/utilities/sync
      * @example
-     *  var syncInstance = altspace.utilities.sync.getInstance({
+     *  var syncInstance = altspace.utilities.sync.getInstanceRef({
      *      // All sync instances with the same instance id will share 
      *      // properties. 
      *      instanceId: yourInstanceId, 
@@ -87,5 +146,12 @@ altspace.utilities.sync = (function() {
      *      authorId: yourAuthorId  
      *  });
      */
-    return { getInstance: getInstance, authenticate: authenticate };
+    return {
+      getInstance: getInstance,
+      getInstanceRef: getInstanceRef,
+      getSpaceRef: getSpaceRef,
+      authenticate: authenticate,
+      getSpaceId: function() { return spaceId; }
+    };
+    
 }());
