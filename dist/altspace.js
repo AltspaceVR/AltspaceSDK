a83ecf83031dc16c8b1e10e8cc06a903e710f37b
altspaceautobot <brian+autobot@altvr.com>
2016-06-01 18:23:10 -0700

    Bump release v0.13.0

--- dist/altspace.js
+++ dist/altspace.js
@@ -11171,7 +11171,7 @@ window.altspace.utilities.behaviors.SteamVRTrackedObject = SteamVRTrackedObjectB
 
 (function () {
 
-	var version = '0.12.1';
+	var version = '0.13.0';
 
 	if (window.altspace && window.altspace.requestVersion) {
 		window.altspace.requestVersion(version);
