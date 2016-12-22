7062a2b5149ef5108f5a111c2186217ec03a8b38
Brian Peiris <brianpeiris@gmail.com>
2016-12-21 17:03:09 -0800

    Display full resource paths in docs

--- src/native-resources.js
+++ src/native-resources.js
@@ -15,64 +15,64 @@
 * @enum architecture
 * @memberof resources
 *
-* @prop ceiling-2w-2l
-* @prop ceiling-4w-4l
-* @prop ceiling-4w-4l
-* @prop ceiling-skylight-4w-4l
-* @prop ceiling-skylight-corner-2w-2l
-* @prop ceiling-skylight-edge-2w
-* @prop ceiling-skylight-edge-4w
-* @prop ceiling-skylight-filler-4w-4l-2
-* @prop ceiling-skylight-filler-4w-4l
-* @prop ceiling-slice-concave-2r
-* @prop ceiling-slice-concave-4r
-* @prop ceiling-slice-convex-2r
-* @prop ceiling-slice-convex-4r
-* @prop door-4w-4h
-* @prop floor-2w-2l
-* @prop floor-2w-4l
-* @prop floor-4w-2l
-* @prop floor-4w-4l
-* @prop floor-slice-concave-2r
-* @prop floor-slice-concave-4r
-* @prop floor-slice-convex-2r
-* @prop floor-slice-convex-4r
-* @prop railing-2l
-* @prop railing-4l
-* @prop railing-curve-concave-2r
-* @prop wall-2w-4h
-* @prop wall-4w-4h
-* @prop wall-base-2w
-* @prop wall-base-4w
-* @prop wall-base-curve-concave-2r
-* @prop wall-base-curve-concave-4r
-* @prop wall-base-curve-convex-2r
-* @prop wall-base-curve-convex-4r
-* @prop wall-bulkhead-2w
-* @prop wall-bulkhead-4w
-* @prop wall-bulkhead-curve-concave-2r
-* @prop wall-bulkhead-curve-concave-4r
-* @prop wall-bulkhead-curve-convex-2r
-* @prop wall-bulkhead-curve-convex-4r
-* @prop wall-curve-concave-2r-4h
-* @prop wall-curve-concave-4r-4h
-* @prop wall-curve-convex-2r-4h
-* @prop wall-curve-convex-4r-4h
-* @prop wall-curve-window-concave-4r-4h
-* @prop wall-curve-window-concave-filler-4r-4h
-* @prop wall-curve-window-gap-concave-4r-4h
-* @prop wall-curve-window-gap-end-l-concave-4r-4h
-* @prop wall-curve-window-gap-end-r-concave-4r-4h
-* @prop wall-filler-corner-inner-4h
-* @prop wall-filler-corner-outer-4h
-* @prop wall-window-4w-4h
-* @prop wall-window-filler-2
-* @prop wall-window-gap-2w-4h
-* @prop wall-window-gap-4w-4h
-* @prop wall-window-gap-end-l-2w-4h
-* @prop wall-window-gap-end-l-4w-4h
-* @prop wall-window-gap-end-r-2w-4h
-* @prop wall-window-gap-end-r-4w-4h
+* @prop architecture/ceiling-2w-2l
+* @prop architecture/ceiling-4w-4l
+* @prop architecture/ceiling-4w-4l
+* @prop architecture/ceiling-skylight-4w-4l
+* @prop architecture/ceiling-skylight-corner-2w-2l
+* @prop architecture/ceiling-skylight-edge-2w
+* @prop architecture/ceiling-skylight-edge-4w
+* @prop architecture/ceiling-skylight-filler-4w-4l-2
+* @prop architecture/ceiling-skylight-filler-4w-4l
+* @prop architecture/ceiling-slice-concave-2r
+* @prop architecture/ceiling-slice-concave-4r
+* @prop architecture/ceiling-slice-convex-2r
+* @prop architecture/ceiling-slice-convex-4r
+* @prop architecture/door-4w-4h
+* @prop architecture/floor-2w-2l
+* @prop architecture/floor-2w-4l
+* @prop architecture/floor-4w-2l
+* @prop architecture/floor-4w-4l
+* @prop architecture/floor-slice-concave-2r
+* @prop architecture/floor-slice-concave-4r
+* @prop architecture/floor-slice-convex-2r
+* @prop architecture/floor-slice-convex-4r
+* @prop architecture/railing-2l
+* @prop architecture/railing-4l
+* @prop architecture/railing-curve-concave-2r
+* @prop architecture/wall-2w-4h
+* @prop architecture/wall-4w-4h
+* @prop architecture/wall-base-2w
+* @prop architecture/wall-base-4w
+* @prop architecture/wall-base-curve-concave-2r
+* @prop architecture/wall-base-curve-concave-4r
+* @prop architecture/wall-base-curve-convex-2r
+* @prop architecture/wall-base-curve-convex-4r
+* @prop architecture/wall-bulkhead-2w
+* @prop architecture/wall-bulkhead-4w
+* @prop architecture/wall-bulkhead-curve-concave-2r
+* @prop architecture/wall-bulkhead-curve-concave-4r
+* @prop architecture/wall-bulkhead-curve-convex-2r
+* @prop architecture/wall-bulkhead-curve-convex-4r
+* @prop architecture/wall-curve-concave-2r-4h
+* @prop architecture/wall-curve-concave-4r-4h
+* @prop architecture/wall-curve-convex-2r-4h
+* @prop architecture/wall-curve-convex-4r-4h
+* @prop architecture/wall-curve-window-concave-4r-4h
+* @prop architecture/wall-curve-window-concave-filler-4r-4h
+* @prop architecture/wall-curve-window-gap-concave-4r-4h
+* @prop architecture/wall-curve-window-gap-end-l-concave-4r-4h
+* @prop architecture/wall-curve-window-gap-end-r-concave-4r-4h
+* @prop architecture/wall-filler-corner-inner-4h
+* @prop architecture/wall-filler-corner-outer-4h
+* @prop architecture/wall-window-4w-4h
+* @prop architecture/wall-window-filler-2
+* @prop architecture/wall-window-gap-2w-4h
+* @prop architecture/wall-window-gap-4w-4h
+* @prop architecture/wall-window-gap-end-l-2w-4h
+* @prop architecture/wall-window-gap-end-l-4w-4h
+* @prop architecture/wall-window-gap-end-r-2w-4h
+* @prop architecture/wall-window-gap-end-r-4w-4h
 */
 
 /**
@@ -81,15 +81,15 @@
 * @enum effects
 * @memberof resources
 *
-* @prop explosion - A particle system with a central flash, then debris flying outward.
+* @prop effects/explosion - A particle system with a central flash, then debris flying outward.
 * This is a non-looping effect.
-* @prop fire - An animated fire particle, suitable for a torch.
-* @prop fire-trail - Fire that trails the entity through space as it moves. Only is visible while an object is in motion
-* @prop fireworks - A compound particle system that shoots up from the entity,
+* @prop effects/fire - An animated fire particle, suitable for a torch.
+* @prop effects/fire-trail - Fire that trails the entity through space as it moves. Only is visible while an object is in motion
+* @prop effects/fireworks - A compound particle system that shoots up from the entity,
 * explodes into colored sparks, then transitions to gold streamers.
-* @prop smoke - Billowing smoke particle system.
-* @prop sparkler - Emits sparks in all directions
-* @prop steam - Small white steam rising upwards
+* @prop effects/smoke - Billowing smoke particle system.
+* @prop effects/sparkler - Emits sparks in all directions
+* @prop effects/steam - Small white steam rising upwards
 */
 
 /**
@@ -98,14 +98,14 @@
 * @enum interactables
 * @memberof resources
 *
-* @prop basketball
-* @prop bowlingball
-* @prop bowling-pin
-* @prop box
-* @prop coin
-* @prop gem
-* @prop ring
-* @prop soccerball
+* @prop interactables/basketball
+* @prop interactables/bowlingball
+* @prop interactables/bowling-pin
+* @prop interactables/box
+* @prop interactables/coin
+* @prop interactables/gem
+* @prop interactables/ring
+* @prop interactables/soccerball
 */
 
 /**
@@ -114,13 +114,13 @@
 * @enum objects
 * @memberof resources
 *
-* @prop basketball-hoop
-* @prop coin
-* @prop cup
-* @prop gem
-* @prop hoop
-* @prop ring
-* @prop target-archery
+* @prop objects/basketball-hoop
+* @prop objects/coin
+* @prop objects/cup
+* @prop objects/gem
+* @prop objects/hoop
+* @prop objects/ring
+* @prop objects/target-archery
 */
 
 /**
@@ -129,22 +129,22 @@
 * @enum pipes
 * @memberof resources
 *
-* @prop pipe-full-cap-1d
-* @prop pipe-full-cross-1d
-* @prop pipe-full-elbow-1d
-* @prop pipe-full-fork-1d
-* @prop pipe-full-straight-1d-1l
-* @prop pipe-full-straight-1d-2l
-* @prop pipe-full-straight-1d-4l
-* @prop pipe-full-tee-1d
-* @prop pipe-half-cap-1d
-* @prop pipe-half-cross-1d
-* @prop pipe-half-elbow-1d
-* @prop pipe-half-fork-1d
-* @prop pipe-half-straight-1d-1l
-* @prop pipe-half-straight-1d-2l
-* @prop pipe-half-straight-1d-4l
-* @prop pipe-half-tee-1d
+* @prop pipes/pipe-full-cap-1d
+* @prop pipes/pipe-full-cross-1d
+* @prop pipes/pipe-full-elbow-1d
+* @prop pipes/pipe-full-fork-1d
+* @prop pipes/pipe-full-straight-1d-1l
+* @prop pipes/pipe-full-straight-1d-2l
+* @prop pipes/pipe-full-straight-1d-4l
+* @prop pipes/pipe-full-tee-1d
+* @prop pipes/pipe-half-cap-1d
+* @prop pipes/pipe-half-cross-1d
+* @prop pipes/pipe-half-elbow-1d
+* @prop pipes/pipe-half-fork-1d
+* @prop pipes/pipe-half-straight-1d-1l
+* @prop pipes/pipe-half-straight-1d-2l
+* @prop pipes/pipe-half-straight-1d-4l
+* @prop pipes/pipe-half-tee-1d
 */
 
 /**
@@ -153,15 +153,15 @@
 * @enum sounds-ui
 * @memberof resources
 *
-* @prop select
-* @prop toggle
-* @prop notify
-* @prop error
-* @prop complete
-* @prop succeed
-* @prop over
-* @prop join
-* @prop click
+* @prop ui/select
+* @prop ui/toggle
+* @prop ui/notify
+* @prop ui/error
+* @prop ui/complete
+* @prop ui/succeed
+* @prop ui/over
+* @prop ui/join
+* @prop ui/click
 */
 
 /**
@@ -170,12 +170,12 @@
 * @enum sounds-foley
 * @memberof resources
 *
-* @prop metal-scrape
-* @prop metal-clack
-* @prop metal-rattle
-* @prop coin-jingle
-* @prop paper-shuffle
-* @prop explode
+* @prop foley/metal-scrape
+* @prop foley/metal-clack
+* @prop foley/metal-rattle
+* @prop foley/coin-jingle
+* @prop foley/paper-shuffle
+* @prop foley/explode
 */
 
 /**
@@ -184,10 +184,10 @@
 * @enum sounds-effects
 * @memberof resources
 *
-* @prop fanfare-succeed - The "success!" sound from Holograms Against Humanity.
-* @prop fanfare-start - The "Game has started!" sound from HaH.
-* @prop fanfare-fail
-* @prop timer-10s - a 10 second timer that triggers a bell at exactly 10 seconds.
+* @prop effects/fanfare-succeed - The "success!" sound from Holograms Against Humanity.
+* @prop effects/fanfare-start - The "Game has started!" sound from HaH.
+* @prop effects/fanfare-fail
+* @prop effects/timer-10s - a 10 second timer that triggers a bell at exactly 10 seconds.
 * The bell lasts for 2 seconds. This allows for timer length changes.
-* @prop gain-coin
+* @prop effects/gain-coin
 */
