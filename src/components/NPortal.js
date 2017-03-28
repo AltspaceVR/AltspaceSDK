a3dd6d55e8acab6fddff8841a1f6a9ee12d60821
Brian Peiris <brianpeiris@gmail.com>
2017-03-28 16:12:41 -0700

    Finish native portals

--- src/components/NPortal.js
+++ src/components/NPortal.js
@@ -14,15 +14,23 @@ class NPortal extends NativeComponent {
 	}
 	update(){
 		let mesh = this.el.object3DMap.mesh;
-		this.el.sceneEl.object3D.updateMatrixWorld(true);
-		let position = this.data.targetEntity.object3D.getWorldPosition();
-		let rotation = this.data.targetEntity.object3D.getWorldRotation();
-		var data = {
-			targetSpace: this.targetSpace,
-			targetPosition: position,
-			targetRotation: rotation
+		let targetPosition, targetQuaternion;
+		if (this.data.targetEntity) {
+			// updateMatrixWorld doesn't traverse upwards to actually update the world matrix.
+			// Updating the entire scene's world matrcies is overkill, but there isn't a simple way to do the right thing
+			// at the moment. See https://github.com/mrdoob/three.js/pull/9410
+			this.el.sceneEl.object3D.updateMatrixWorld(true);
+			targetPosition = this.data.targetEntity.object3D.getWorldPosition();
+			let quaternion = this.data.targetEntity.object3D.getWorldQuaternion();
+			targetQuaternion = {x: quaternion.x, y: quaternion.y, z: quaternion.z, w: quaternion.w};
 		}
-		console.log('BPDEBUG nportal data ', JSON.stringify(data.targetPosition));
+
+		var data = {
+			targetSpace: this.data.targetSpace,
+			targetPosition: targetPosition,
+			targetQuaternion: targetQuaternion
+		}
+
 		altspace.updateNativeComponent(mesh, this.name, data);
 	}
 }
