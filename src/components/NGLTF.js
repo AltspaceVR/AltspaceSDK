22f37ab66afc58ba53f5f9574b85f21adeff78e2
Steven Vergenz <steven@altvr.com>
2017-05-22 14:36:09 -0700

    Update to latest api

--- src/components/NGLTF.js
+++ src/components/NGLTF.js
@@ -1,10 +1,21 @@
 import NativeComponent from './NativeComponent';
+import Url from 'urllib';
 
-export default class NSphereCollider extends NativeComponent {
+export default class NGLTF extends NativeComponent {
 	constructor() {super('n-gltf'); }
 	get schema() {
 		return {
 			url: { type: 'string' },
+			sceneIndex: { type: 'int' }
 		};
+	}
+	update(){
+		let mesh = this.mesh || this.el.object3DMap.mesh;
+		var data = Object.assign({}, this.data);
+		data.url = new Url(data.url).toString();
+
+		if(this.sendUpdates){
+			altspace.updateNativeComponent(mesh, this.name, data);
+		}
 	}
 }
