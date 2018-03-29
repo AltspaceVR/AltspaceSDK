50a15ee0329f8f9c97e5d8380a1ffe3c4fb7bc2f
Steven Vergenz <Steven.Vergenz@microsoft.com>
2018-03-29 16:54:59 -0700

    Update documentation for Users api

--- src/core/User.js
+++ src/core/User.js
@@ -1,11 +1,29 @@
 'use strict';
 
 /**
-* Details of a user.
-* @typedef {Object} module:altspace~User
-* @property {String} userId Unique id for this user.
-* @property {String} displayName The user's chosen display name.
-* @property {Boolean} isModerator True if the user should be able to moderate apps
+* Details of an Altspace user.
+* @class module:altspace~User
+* @memberof module:altspace
+* @property {String} userId - Unique id for this user (per site origin)
+* @property {String} displayName - The user's chosen display name.
+* @property {Boolean} isModerator - True if the user should be able to moderate apps.
+* @property {Object} avatarInfo - What avatar the user is wearing
+* @property {String} avatarInfo.sid - The series ID of the avatar. Will be one of:
+*   "rubenoid-male-01" (human male), "rubenoid-female-01" (human female),
+*   "s-series-m01" (robot male), "s-series-f01" (robot female),
+*   "pod-classic" (rounded robot), "a-series-m01" (swirled robot), "x-series-m02" (visor robot),
+*   "robothead-propellerhead-01" (helicopter robot), "robothead-roundguy-01" (bumblebee robot),
+*   or other future avatar models.
+* @property {String} avatarInfo.primaryColor - The main color of the avatar (s, a, x, and pod series only)
+* @property {String} avatarInfo.highlightColor - The accent color of the avatar (s, a, x, and pod series only)
+* @property {Array} avatarInfo.textures - Texture selection IDs (rubenoids only)
+*/
+
+/**
+* The avatarchange event is fired when the user changes their avatar.
+* @event avatarchange
+* @memberof module:altspace~User
+* @property {Object} data - The new avatar info, in the same format as User.avatarInfo
 */
 
 /**
